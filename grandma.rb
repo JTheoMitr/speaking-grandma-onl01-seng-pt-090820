83c83cc5136947fc678ba9d16f3dfede0fa18cfb
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-10 07:00:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,6 @@
 
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU, GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif greeting == greeting.upcase
     puts "NO, NOT SINCE 1938!"
